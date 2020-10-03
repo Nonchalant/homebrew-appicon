e9a28086842ea0df7bd0b1c0ea8370b5647e851d
Nonchalant <afrontier829@gmail.com>
2020-10-03 20:58:29 +0900

    1.0.5

--- appicon.rb
+++ appicon.rb
@@ -1,16 +1,16 @@
 class Appicon < Formula
   desc "Generation of iOS AppIcon.appiconset made easy"
   homepage "https://github.com/Nonchalant/AppIcon"
-  url "https://github.com/Nonchalant/AppIcon/archive/1.0.4.tar.gz"
-  sha256 "c97832ed6994550c7f439000a7b6ea1ffa495a23e7dbad9aa5cf1ba77b541aec"
+  url "https://github.com/Nonchalant/AppIcon/archive/1.0.5.tar.gz"
+  sha256 "dfd0239216ea6dcbf3ea99f599d144b6d8bf6e854d594e6ec93d34eaa8c28e09"
 
   head 'https://github.com/Nonchalant/AppIcon.git', :branch => 'master'
 
-  depends_on :xcode => ["10.1", :build]
+  depends_on :xcode => ["12", :build]
 
   bottle do
-    root_url "https://github.com/Nonchalant/AppIcon/releases/download/1.0.4"
-    sha256 "9ca60898e315454c346837a40eef10f0d75c78c4d45f9d6246aa842f8a9fa069" => :catalina
+    root_url "https://github.com/Nonchalant/AppIcon/releases/download/1.0.5"
+    sha256 "ccf261cf9206410bc1adf77996bc3c4461d3199de75e65193e755504f98cfb6e" => :catalina
   end
 
   def install
