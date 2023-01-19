6e22774152aafc49e74e970589e4a366c22079a8
Nonchalant <afrontier829@gmail.com>
2023-01-20 00:43:57 +0900

    1.0.6

--- appicon.rb
+++ appicon.rb
@@ -5,17 +5,17 @@
 class Appicon < Formula
   desc "Generation of iOS AppIcon.appiconset made easy"
   homepage "https://github.com/Nonchalant/AppIcon"
-  url "https://github.com/Nonchalant/AppIcon/archive/1.0.5.tar.gz"
-  sha256 "dfd0239216ea6dcbf3ea99f599d144b6d8bf6e854d594e6ec93d34eaa8c28e09"
+  url "https://github.com/Nonchalant/AppIcon/archive/1.0.6.tar.gz"
+  sha256 "723ff3349081916c22f3cbd29f328113a28858a898f84fdc8b8d3e4329fc31f2"
 
   head "https://github.com/Nonchalant/AppIcon.git", branch: "master"
 
   bottle do
-    root_url "https://github.com/Nonchalant/AppIcon/releases/download/1.0.5"
-    sha256 catalina: "ccf261cf9206410bc1adf77996bc3c4461d3199de75e65193e755504f98cfb6e"
+    root_url "https://github.com/Nonchalant/AppIcon/releases/download/1.0.6"
+    sha256 ventura: "29d838a754c1916372c7a48f75286aa2174d9d0335e3a8e954ced7065cf2b1e0"
   end
 
-  depends_on xcode: ["12", :build]
+  depends_on xcode: ["14", :build]
 
   def install
     system "make", "install", "PREFIX=#{prefix}"
